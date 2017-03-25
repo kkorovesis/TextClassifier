4fecfdc90cad75d5518326fc69998684872e6780
kvothe88 <kkorovesis88@gmail.com>
2017-03-25 19:49:35 +0200

    test parsers

--- parsers/enronhamparser.py
+++ parsers/enronhamparser.py
@@ -1,9 +1,10 @@
-import os
+import os, codecs
 from email.parser import Parser
 
-ham_rootdir = "C:\\Corpus\\Enron Ham"
+# ham_rootdir = "C:\\Corpus\\Enron Ham"
 # ham_rootdir = "C:\\Corpus\\Enron Ham\\farmer-d"
 # ham_rootdir = "C:\\Corpus\\Enron Ham\\kaminski-v\\azurix_azurix"
+ham_rootdir = "C:\Corpus\CSDMC2010_SPAM\TRAINING_NEW"
 
 spam_rootdir = ""
 def email_lebelling(email_labels,label):
@@ -11,7 +12,7 @@ def email_lebelling(email_labels,label):
 
 # Email Analuzer
 def email_analyse(inputfile, to_email_list, from_email_list, email_body):
-    with open(inputfile, "r") as f:
+    with codecs.open(inputfile, mode='r', encoding='utf-8', errors='ignore') as f:
         data = f.read()
 
     email = Parser().parsestr(data)
@@ -26,14 +27,10 @@ subject_email_list = []
 email_body = []
 email_label = []
 
+
 # Get All ham mails in directory, Ham Label = 0
 for directory, subdirectory, filenames in  os.walk(ham_rootdir):
     for filename in filenames:
         email_analyse(os.path.join(directory, filename), to_email_list, from_email_list, email_body)
         email_lebelling(email_label,0)
 
-# Get All spam mails in directory, Ham Label = 1
-for spam_directory, spam_subdirectory, filenames in  os.walk(spam_rootdir):
-    for filename in filenames:
-        email_analyse(os.path.join(spam_directory, filename), to_email_list, from_email_list, email_body)
-        email_lebelling(email_label,1)
